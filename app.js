34f40a2bec4713ebef41ccde863642d5911adb02
bedru mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-12-09 11:32:52 +0300

    building the middle ware

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import {  PORT} from './config/env.js'
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app=express();
- 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
+
+ app.use(errorMiddleware)
+
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/user',userRouter);
 app.use('/api/v1/subscription',subscriptionRouter);
